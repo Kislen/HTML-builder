67fcf02718d099e89037fd9c88e2b2e163c6e370
Kisliankou Viachaslau <kislenkov.vs@gmail.com>
2023-05-05 19:32:38 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyDir(src, aim) {
+  src = path.join(__dirname, src);
+  aim = path.join(__dirname, aim);
+  try {
+    await fs.access(aim);
+  } catch (error) {
+    await fs.mkdir(aim);
+  }
+  const files = await fs.readdir(src);
+  for (let file of files) {
+    const currentSrc = path.join(src, file);
+    const currentAim = path.join(aim, file);
+    const fileStat = await fs.stat(currentSrc);
+    if (fileStat.isDirectory()) {
+      await copyDir(currentSrc, currentAim);
+    } else {
+      await fs.copyFile(currentSrc, currentAim);
+    }
+  }
+}
+
+copyDir('files', 'files-copy')
+  .then(() => console.log('Directory copied successfully'))
+  .catch((err) => console.log(err));
+
+
+
+
+
 
